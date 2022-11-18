2f362bf3ae477b0d52135bca84e63e7a33985af0
lagushonok2000 <vorobjeva.an4stasiy@yandex.ru>
2022-11-18 15:44:46 +0300

    ChangedHomework5

--- Assets/Breaker.cs
+++ Assets/Breaker.cs
@@ -1,108 +1,89 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Breaker : MonoBehaviour
 {
-    public GameObject PanelVictory;
-    public GameObject PanelDefeat;
-    public Text A;
-    public Text B; 
-    public Text C;
-    public Text Timer;
-    public Button DrillButton;
-    public Button HammerButton;
-    public Button MasterkeyButton;
-    private float _time = 0;
-    private float _timeNumber = 60;
-    private int _a = 4;
-    private int _b = 5;
-    private int _c = 6;
+    [SerializeField] private GameObject _panelVictory;
+    [SerializeField] private GameObject _panelDefeat;
+    [SerializeField] private Text _textPinNumber1;
+    [SerializeField] private Text _textPinNumber2;
+    [SerializeField] private Text _textPinNumber3;
+    [SerializeField] private Text _timer;
+    [SerializeField] private Button _drillButton;
+    [SerializeField] private Button _hammerButton;
+    [SerializeField] private Button _masterkeyButton;
+    [SerializeField] private Button _restartVictoryButton;
+    [SerializeField] private Button _restartDefeatButton;
 
+    private float _timeNumber = 10;
+    private int _digitPinNumber1 = 4;
+    private int _digitPinNumber2 = 5;
+    private int _digitPinNumber3 = 6;
+    private int _victoryDigit = 5;
+    private int _minDigit = 0;
+    private int _maxDigit = 9;
 
-    // 3 хода. Изначальное 5 5 5 Hammer(+1,-2,+1), Drill(-1,+1), Drill(-1,+1) 4 5 6
     void Start()
     {
-        
+        _restartVictoryButton.onClick.AddListener(Restart);
+        _restartDefeatButton.onClick.AddListener(Restart);
+        _drillButton.onClick.AddListener(() => ButtonMethod(1, -1, 0));
+        _hammerButton.onClick.AddListener(() => ButtonMethod(-1, 2, -1));
+        _masterkeyButton.onClick.AddListener(() => ButtonMethod(-1, 1, 1));
+
+        StartCoroutine(Timer());
     }
-
-    // Update is called once per frame
-    void Update()
-    { 
-        _time = _time + Time.deltaTime;
-        A.text = Convert.ToString(_a);
-        B.text = Convert.ToString(_b);
-        C.text = Convert.ToString(_c);
-
-        if (PanelVictory.active == false && PanelDefeat.active == false)
-        {
-            if (_timeNumber - Mathf.Round(_time) >= 1)
-            {
-                Timer.text = Convert.ToString(_timeNumber - Mathf.Round(_time));
-
-            }
-            else
-            {
-                Timer.text = "Время вышло";
-                PanelDefeat.SetActive(true);
-            }
-
-        }
-        else
-        {
-            DrillButton.interactable = false;
-            MasterkeyButton.interactable = false;
-            HammerButton.interactable = false;
-        }
-
-    }
-
-    public void Drill()
+   
+    private IEnumerator Timer()
     {
-        if (_a + 1 > 9 || _b - 1 < 0) return;
+        while (_timeNumber > 0)
+        {
+            _timer.text = Convert.ToString(_timeNumber);
+            _timeNumber--;
+            yield return new WaitForSeconds(1);
+        }
         
-        _a = _a + 1;
-        _b = _b - 1;
+        _timer.text = "Время вышло";
+        _panelDefeat.SetActive(true);
 
-        if (_a == 5 && _b == 5 && _c == 5)
-        {
-            PanelVictory.SetActive(true);
-        }
+        DeactivateButton();
     }
 
-    public void Hammer()
+    private void ButtonMethod(int digitNumber1, int digitNumber2, int digitNumber3)
     {
-        if (_a - 1 < 0 || _b + 2 > 9 || _c - 1 < 0) return;
-        _a = _a - 1;
-        _b = _b + 2;
-        _c = _c - 1;
+        if ((_digitPinNumber1 + digitNumber1 > _maxDigit || _digitPinNumber1 + digitNumber1 < _minDigit) ||
+           (_digitPinNumber2 + digitNumber2 > _maxDigit || _digitPinNumber2 + digitNumber2 < _minDigit) ||
+           (_digitPinNumber3 + digitNumber3 > _maxDigit || _digitPinNumber3 + digitNumber3 < _minDigit))
+           return; 
+     
+        _digitPinNumber1 += digitNumber1;
+        _digitPinNumber2 += digitNumber2;
+        _digitPinNumber3 += digitNumber3;
 
-        if (_a == 5 && _b == 5 && _c == 5)
+        _textPinNumber1.text = Convert.ToString(_digitPinNumber1);
+        _textPinNumber2.text = Convert.ToString(_digitPinNumber2);
+        _textPinNumber3.text = Convert.ToString(_digitPinNumber3);
+
+        if (_digitPinNumber1 == _victoryDigit && _digitPinNumber2 == _victoryDigit && _digitPinNumber3 == _victoryDigit)
         {
-            PanelVictory.SetActive(true);
-
+            _panelVictory.SetActive(true);
+            DeactivateButton();
+            StopAllCoroutines();
         }
     }
 
-    public void MasterKey()
+    private void DeactivateButton()
     {
-        if (_a - 1 < 0 || _b + 1 > 9 || _c + 1 > 9) return;
-        _a = _a - 1;
-        _b = _b + 1;
-        _c = _c + 1;
-        
-        if (_a == 5 && _b == 5 && _c == 5)
-        {
-            PanelVictory.SetActive(true);
-        }
+        _drillButton.interactable = false;
+        _masterkeyButton.interactable = false;
+        _hammerButton.interactable = false;
     }
 
-    public void Restart()
+    private void Restart()
     {
         SceneManager.LoadScene(0);
     }
-
 }
